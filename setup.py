0c0a212bc621bee4c17f677b2c0df89628f487cd
lxmcneill <lxmcneill@gmail.com>
2019-06-19 16:34:55 +1000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="Astutus",
+    version="0.1.0",
+    author="Alex McNeill",
+    license="LICENSE",
+    long_description=open("README.md").read(),
+    install_requires=[],
+)
