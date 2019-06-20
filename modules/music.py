0c4c3087c20531f43437ee46f1354678ec17804e
lxmcneill <lxmcneill@gmail.com>
2019-06-21 00:15:01 +1000

    Minor music tweak

--- modules/music.py
+++ modules/music.py
@@ -94,7 +94,14 @@ class YTDLSource(discord.PCMVolumeTransformer):
         to_run = partial(YTDL.extract_info, url=data["webpage_url"], download=False)
         data = await loop.run_in_executor(None, to_run)
 
-        return cls(discord.FFmpegPCMAudio(data["url"]), data=data, requester=requester)
+        return cls(
+            discord.FFmpegPCMAudio(
+                data["url"],
+                before_options="-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",
+            ),
+            data=data,
+            requester=requester,
+        )
 
 
 class MusicPlayer:
