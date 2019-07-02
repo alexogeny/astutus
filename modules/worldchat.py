2d923986d305292a1d59777e14bf02ab00ef2608
lxmcneill <lxmcneill@gmail.com>
2019-07-02 16:33:44 +1000

    Fix up world chat including cc scanning and newline filters

--- modules/worldchat.py
+++ modules/worldchat.py
@@ -3,6 +3,19 @@ import arrow
 import discord
 from discord.ext import commands as cmd
 from .utils import checks
+import re
+
+URL = re.compile(
+    r"(?:(?:https?|ftp|file|mailto):?)(?://)?(?:[-\w.]|(?:%[\da-fA-F]{2}))+|(?:[\w:]+\@\w+\.\w+)"
+)
+VISA = re.compile(r"(4[0-9]{3})[\W\s-]?([0-9]{4}[\W\s-]?){3}")
+MSTR = re.compile(
+    r"(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}"
+)
+AMEX = re.compile(r"3[47][0-9]{13}")
+DNRS = re.compile(r"3(?:0[0-5]|[68][0-9])[0-9]{11}")
+DSCV = re.compile(r"6(?:011|5[0-9]{2})[0-9]{12}")
+RET = re.compile(r"\n\n")
 
 
 class WorldChatModule(cmd.Cog):
@@ -12,6 +25,9 @@ class WorldChatModule(cmd.Cog):
     async def wc_enabled(self, message):
         if not hasattr(message, "guild") or message.author.bot:
             return False
+        log_is_on = await self.bot.db.hget(f"{message.guild.id}:toggle", "worldchat")
+        if log_is_on in (None, "0"):
+            return
         ctx = await self.bot.get_context(message)
         if ctx.valid:
             return False
@@ -33,13 +49,18 @@ class WorldChatModule(cmd.Cog):
     async def on_message(self, message):
         if not message.content or message.content is None:
             return
+        any_match = [
+            X.findall(message.content) for X in [URL, VISA, MSTR, AMEX, DNRS, DSCV]
+        ]
+        if any(any_match):
+            return
         if not await self.wc_enabled(message):
             return
 
         embed = await self.bot.embed()
         embed.title = str(message.author)
 
-        embed.description = message.content[0:300]
+        embed.description = RET.sub("", message.content[0:300]).replace("\n", " ")
 
         chats = await self.bot.db.hgetall("worldchat")
         censored = self.bot.profanity.censor(embed.description, "\\*")
